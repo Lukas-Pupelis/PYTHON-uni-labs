e7d20f95d05cd38816599d8721d8058bb212f591
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2024-06-05 13:42:37 +0300

    added html template folder with html files and mapped them in the add urls file

---
+++ 5/myproject/lupu/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create/', views.create_invoice, name='create_invoice'),
+    path('list/', views.invoice_list, name='invoice_list'),
+    path('pdf/<int:invoice_id>/', views.generate_pdf, name='generate_pdf'),
+]
