a4f763f96ac6d275909e605937b2025be05e32c8
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2024-06-05 13:35:15 +0300

    added lupu app into settings and added an invoice model into models.py

--- 5/myproject/lupu/models.py
+++ 5/myproject/lupu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class VATInvoice(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    invoice_number = models.CharField(max_length=50)
+    date = models.DateField()
+    customer_name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+
+    @property
+    def price(self):
+        return self.amount / self.quantity
