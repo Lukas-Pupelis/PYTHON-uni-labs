aed4eedc6fe069cf6c823909f5b80f0c6da18cf6
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2024-06-05 14:19:41 +0300

    modified settings file and views file to map templates correctly.

--- 5/myproject/lupu/views.py
+++ 5/myproject/lupu/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 from .forms import VATInvoiceForm
 from .models import VATInvoice
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
-
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login as auth_login
 
 @login_required
 def create_invoice(request):
@@ -24,17 +19,17 @@ def create_invoice(request):
             return redirect('invoice_list')
     else:
         form = VATInvoiceForm()
-    return render(request, 'lupu/create_invoice.html', {'form': form})
+    return render(request, 'create_invoice.html', {'form': form})
 
 @login_required
 def invoice_list(request):
     invoices = VATInvoice.objects.filter(user=request.user)
-    return render(request, 'lupu/invoice_list.html', {'invoices': invoices})
+    return render(request, 'invoice_list.html', {'invoices': invoices})
 
 @login_required
 def generate_pdf(request, invoice_id):
     invoice = VATInvoice.objects.get(id=invoice_id)
-    template_path = 'lupu/invoice_pdf.html'
+    template_path = 'invoice_pdf.html'
     context = {'invoice': invoice}
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'attachment; filename="invoice_{invoice_id}.pdf"'
